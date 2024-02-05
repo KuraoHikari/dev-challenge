85f526205ff2730fa387f9e2e6b04d5fc53c0b03
KuraoHikari <dewaindra705@gmail.com>
2024-02-05 12:26:40 +0800

    update all

--- app/minimal-blog-card/page.tsx
+++ app/minimal-blog-card/page.tsx
@@ -1,26 +1,45 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
 const Home = () => {
- return (
-  <div>
-   {/* Card */}
-   <div className="card">
-    {/* Item Image */}
-    <div className="image"></div>
-    {/* Item Category */}
-    <div className="category"></div>
-    {/* Item Desc */}
-    <div>
-     {/* Item Title */}
-     <h2></h2>
-     {/* Item Desc */}
-     <p></p>
+  return (
+    <div className={styles.container}>
+      {/* Card */}
+      <div className={styles.card}>
+        {/* Item Image */}
+        <div className={styles.image}>
+          <Image
+            src="/minimal-blog-card/cactus_img.jpg"
+            alt="Cactus image"
+            width={336}
+            height={152}
+          ></Image>
+        </div>
+        {/* Item Category */}
+        <div className={styles.category}>
+          <button>Design</button>
+        </div>
+
+        {/* Item Title */}
+        <div className={styles.cardTitle}>
+          <h2>Embracing Minimalism</h2>
+        </div>
+
+        {/* Item Desc */}
+        <div className={styles.cardDesc}>
+          <p>
+            From minimalist sculptures to minimalist paintings, this blog will
+            inspire you to appreciate the beauty that lies in simplicity.
+          </p>
+        </div>
+
+        {/* Author */}
+        <div className={styles.cardAuthor}>
+          <p>Annie Spratt</p>
+        </div>
+      </div>
     </div>
-    {/* Author */}
-    <div>
-     <p></p>
-    </div>
-   </div>
-  </div>
- );
+  );
 };
 
 export default Home;
