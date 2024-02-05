5f0432ae00ab4a4e592e031a4147b7989c8bc535
KuraoHikari <dewaindra705@gmail.com>
2024-02-05 11:19:24 +0800

    update

---
+++ app/minimal-blog-card/page.tsx
@@ -0,0 +1,26 @@
+const Home = () => {
+ return (
+  <div>
+   {/* Card */}
+   <div className="card">
+    {/* Item Image */}
+    <div className="image"></div>
+    {/* Item Category */}
+    <div className="category"></div>
+    {/* Item Desc */}
+    <div>
+     {/* Item Title */}
+     <h2></h2>
+     {/* Item Desc */}
+     <p></p>
+    </div>
+    {/* Author */}
+    <div>
+     <p></p>
+    </div>
+   </div>
+  </div>
+ );
+};
+
+export default Home;
