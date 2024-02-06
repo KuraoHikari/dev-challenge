4b3dd4a7c17a761ef13c82761233b5bf2cc93956
KuraoHikari <dewaindra705@gmail.com>
2024-02-06 19:17:24 +0800

    finish body card

---
+++ app/business-blog-card/page.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const Home = () => {
+ return (
+  <div className={styles.page}>
+   <div className={styles.card}>
+    {/* Card Body */}
+    <div className={styles.cardBody}>
+     {/* Card Image */}
+     <div className={styles.cardHeroImage}>
+      <Image
+       src="/business-blog-card/hero-image-business-card.png"
+       alt=""
+       width={836}
+       height={560}
+       className={styles.cardImage}
+      />
+      <Image
+       src="/business-blog-card/white-overlay.svg"
+       alt=""
+       width={450}
+       height={75}
+       className={styles.cardImageSVG}
+      />
+     </div>
+
+     {/* Card Text */}
+     <div className={styles.cardText}>
+      <h1>Perfect solution for small business</h1>
+
+      <p>Small businesses need to generate leads to grow. You can use tools like Ringy.</p>
+     </div>
+    </div>
+    {/* Card Footer */}
+    <div className={styles.cardFooter}>
+     <div></div>
+     <div className={styles.cardFooterAuthor}>
+      <h2>Amy Burgess</h2>
+
+      <p>Customer Manger, Solution Oy</p>
+     </div>
+    </div>
+   </div>
+  </div>
+ );
+};
+
+export default Home;
